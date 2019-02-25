eedc51a8552c145ba0813327293900377104f6b9
FX-Wood <fxwood2@gmail.com>
2019-02-25 15:43:58 -0800

    adding 3 routes for posts

--- views/routes/posts.js
+++ views/routes/posts.js
@@ -3,11 +3,45 @@ const db = require('../models');
 const router = express.router();
 
 // POST /posts - create one post
+router.post('/', function(req,res) {
+    db.post.create({
+        title: req.body.title,
+        content: req.body.content,
+        author_id: req.body.authorId
+    })
+    .then(data => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        res.status(500).render('main/500')
+    })
+})
 
 // GET /posts/new - send the form for a post
+router.get('/new', function(req,res) {
+    db.author.findAll()
+        .then(function(authors) {
+            res.render('posts/new', {authors});
+        })
+        .catch(err => {
+            res.status(500).render('main/500');
+        })
+})
 
 // GET /posts/id - read one post
-
+router.get('/:id', (req,res) => {
+    db.post.findOne({
+        where: {id: req.params.id},
+        include: [db.author]
+    })
+    .then(post => {
+        if (!post) throw Error();
+        res.render('show', {post})
+    })
+    .catch(err => {
+        res.send("status(500).render('main/404')")
+    })
+})
 // PUT /posts/:id - update one post
 
 // DELETE /posts/:id - remove one post
