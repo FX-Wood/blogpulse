3f4a4224e074406f310f41d99e2ff2379ed3aef6
FX-Wood <fxwood2@gmail.com>
2019-02-25 15:56:43 -0800

    added routes for authors

---
+++ views/routes/authors.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const db = require('../models');
+const router = express.router();
+
+
+
+// GET /authors - reads all authors
+router.get('/', (req,res) => {
+    db.authors.findAll()
+        .then(authors => {
+            if (!authors) throw Error();
+            res.render('index', {authors});
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
+
+// POST /authors - creates a new author
+router.post('/', (req, res) => {
+    db.author.create({
+        name: req.body.name
+    })
+    .then(author => {
+        if (!author) throw Error();
+        res.redirect('/authors')
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
+// GET /authors/new - gets form for a new author
+router.get('/new', (req, res) => {
+    res.render('authors/new')
+})
+
+// GET /authors/:id - show one author and their posts
+router.get('/authors/:id', (req, res) => {
+    db.findOne({
+        where: {id: req.params.id},
+        include: [db.post]
+    })
+    .then(author => {
+        if (!author) throw Error();
+        res.render('authors/show', {author})
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
+module.exports = router;
+
