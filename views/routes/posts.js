c822dac88fedf9ee46b378007542b0fb4bbe2039
FX-Wood <fxwood2@gmail.com>
2019-02-25 15:20:08 -0800

    Adding boilerplate and pseudocode

---
+++ views/routes/posts.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const db = require('../models');
+const router = express.router();
+
+// POST /posts - create one post
+
+// GET /posts/new - send the form for a post
+
+// GET /posts/id - read one post
+
+// PUT /posts/:id - update one post
+
+// DELETE /posts/:id - remove one post
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
